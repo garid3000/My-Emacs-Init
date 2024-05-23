3454f094020d78cf44cb29c10a14fb9a1656f824
garid.z <garidzorigoo@gmail.com>
2024-05-23 19:55:32 +0900

    small test

--- exwm-conf.el
+++ exwm-conf.el
@@ -1,4 +1,11 @@
 
+(require 'exwm)
+(require 'exwm-config)
+(exwm-config-example)
+
+;(exwm-enable)
 (setq exwm-manage-configurations
       '(((member exwm-class-name '("kitty" "firefox"))
          char-mode t)))
+
+(display-battery-mode)
